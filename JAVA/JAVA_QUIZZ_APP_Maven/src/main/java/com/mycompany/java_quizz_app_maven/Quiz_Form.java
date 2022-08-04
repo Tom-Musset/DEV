b6bd8c70ff8b48eb58ff5610d79f9c3672d421a2
tom musset <tommusset@hotmail.fr>
2022-08-04 14:02:32 +0200

    dvv

--- JAVA/JAVA_QUIZZ_APP_Maven/src/main/java/com/mycompany/java_quizz_app_maven/Quiz_Form.java
+++ JAVA/JAVA_QUIZZ_APP_Maven/src/main/java/com/mycompany/java_quizz_app_maven/Quiz_Form.java
@@ -4,19 +4,60 @@
  */
 package com.mycompany.java_quizz_app_maven;
 
+import java.awt.Color;
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
+
 /**
  *
  * @author TOM
  */
 public class Quiz_Form extends javax.swing.JFrame {
 
+    
+    String[] questions = {"question 1 ?","question 2 ?","question 3 ?","question 4 ?","question 5 ?"};
+    // the last item is the correct answer
+    
+    String[][] options = {{"0","1","2","3"},{"","","",""},{"","","",""},{"","","",""},{"","","",""}};
+    int index = 0, correct = 0;
+    
+    ButtonGroup bg = new ButtonGroup();
     /**
      * Creates new form Quiz_Form
      */
     public Quiz_Form() {
         initComponents();
+        
+        bg.add(jRadioButton1_);
+        bg.add(jRadioButton2_);
+        bg.add(jRadioButton3_);
+        bg.add(jRadioButton4_);
+        
+        jButton_Next_QActionPerformed(null);
     }
 
+    public void getSelectedOption(JRadioButton rbtn){
+        // get the selected value from the radiobutton
+        // increment the index
+        // disable the radiobuttons
+        // check if the user selected the right answer
+        System.out.println(rbtn.getText());
+        System.out.println(options[index][3]);
+        if(rbtn.getText().equals(options[index][3])){
+            correct++;
+        }
+        index++;
+        enableRbuttons(false);
+    }
+    
+    // enable/disable radiobuttons
+    public void enableRbuttons(boolean yes_or_no){
+        jRadioButton1_.setEnabled(yes_or_no);
+        jRadioButton2_.setEnabled(yes_or_no);
+        jRadioButton3_.setEnabled(yes_or_no);
+        jRadioButton4_.setEnabled(yes_or_no);
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -26,21 +67,192 @@ public class Quiz_Form extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jPanel_Q_Container = new javax.swing.JPanel();
+        Lbl_Question = new javax.swing.JLabel();
+        jRadioButton1_ = new javax.swing.JRadioButton();
+        jRadioButton2_ = new javax.swing.JRadioButton();
+        jRadioButton3_ = new javax.swing.JRadioButton();
+        jRadioButton4_ = new javax.swing.JRadioButton();
+        jButton_Next_Q = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(470, 425));
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        jPanel_Q_Container.setBackground(new java.awt.Color(204, 204, 204));
+
+        Lbl_Question.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        Lbl_Question.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        Lbl_Question.setText("Question here ??");
+
+        javax.swing.GroupLayout jPanel_Q_ContainerLayout = new javax.swing.GroupLayout(jPanel_Q_Container);
+        jPanel_Q_Container.setLayout(jPanel_Q_ContainerLayout);
+        jPanel_Q_ContainerLayout.setHorizontalGroup(
+            jPanel_Q_ContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel_Q_ContainerLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(Lbl_Question, javax.swing.GroupLayout.PREFERRED_SIZE, 430, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(14, Short.MAX_VALUE))
+        );
+        jPanel_Q_ContainerLayout.setVerticalGroup(
+            jPanel_Q_ContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel_Q_ContainerLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(Lbl_Question, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(16, Short.MAX_VALUE))
+        );
+
+        jRadioButton1_.setText("jRadioButton1");
+        jRadioButton1_.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton1_ActionPerformed(evt);
+            }
+        });
+
+        jRadioButton2_.setText("jRadioButton1");
+        jRadioButton2_.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton2_ActionPerformed(evt);
+            }
+        });
+
+        jRadioButton3_.setText("jRadioButton1");
+        jRadioButton3_.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton3_ActionPerformed(evt);
+            }
+        });
+
+        jRadioButton4_.setText("jRadioButton1");
+        jRadioButton4_.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton4_ActionPerformed(evt);
+            }
+        });
+
+        jButton_Next_Q.setBackground(new java.awt.Color(255, 153, 51));
+        jButton_Next_Q.setForeground(new java.awt.Color(255, 255, 255));
+        jButton_Next_Q.setText("Next");
+        jButton_Next_Q.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_Next_QActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jRadioButton1_, javax.swing.GroupLayout.DEFAULT_SIZE, 450, Short.MAX_VALUE)
+                            .addComponent(jRadioButton2_, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jRadioButton3_, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jRadioButton4_, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jPanel_Q_Container, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 8, Short.MAX_VALUE)))
+                .addContainerGap())
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(166, 166, 166)
+                .addComponent(jButton_Next_Q, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel_Q_Container, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jRadioButton1_)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jRadioButton2_)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jRadioButton3_)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jRadioButton4_)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 72, Short.MAX_VALUE)
+                .addComponent(jButton_Next_Q, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(28, 28, 28))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton_Next_QActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_Next_QActionPerformed
+       
+        if(jButton_Next_Q.getText().equals("restart the quiz")){
+            //restart the quiz
+            index = 0 ;
+            correct = 0;
+        }
+        
+        if(index == questions.length){
+            //display the user score
+            Lbl_Question.setText("Your score: " + correct + "/" + questions.length);
+            if(correct >= (float) questions.length/2)
+            {
+                jPanel_Q_Container.setBackground(Color.green);
+            }else{
+                jPanel_Q_Container.setBackground(Color.red);
+            }
+            
+            jButton_Next_Q.setText("restart the quiz");
+        }
+        else{
+             //enable radio buttons
+                enableRbuttons(true);
+
+                // display the next question
+                Lbl_Question.setText(questions[index]);
+                jRadioButton1_.setText(options[index][0]);
+                jRadioButton2_.setText(options[index][1]);
+                jRadioButton3_.setText(options[index][2]);
+                jRadioButton4_.setText(options[index][3]);
+
+                if(index == questions.length-1){
+                    jButton_Next_Q.setText("Finish and see the result");
+                }
+        }
+        
+                // clear the selection
+               bg.clearSelection();
+    }//GEN-LAST:event_jButton_Next_QActionPerformed
+
+    private void jRadioButton1_ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton1_ActionPerformed
+        getSelectedOption(jRadioButton1_);
+    }//GEN-LAST:event_jRadioButton1_ActionPerformed
+
+    private void jRadioButton2_ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton2_ActionPerformed
+        getSelectedOption(jRadioButton2_);
+    }//GEN-LAST:event_jRadioButton2_ActionPerformed
+
+    private void jRadioButton3_ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton3_ActionPerformed
+        getSelectedOption(jRadioButton3_);
+
+    }//GEN-LAST:event_jRadioButton3_ActionPerformed
+
+    private void jRadioButton4_ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton4_ActionPerformed
+        getSelectedOption(jRadioButton4_);
+
+    }//GEN-LAST:event_jRadioButton4_ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +290,13 @@ public class Quiz_Form extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Lbl_Question;
+    private javax.swing.JButton jButton_Next_Q;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel_Q_Container;
+    private javax.swing.JRadioButton jRadioButton1_;
+    private javax.swing.JRadioButton jRadioButton2_;
+    private javax.swing.JRadioButton jRadioButton3_;
+    private javax.swing.JRadioButton jRadioButton4_;
     // End of variables declaration//GEN-END:variables
 }
